b942c15151d571c178247e4161d014f6119a14db
KPGuerra <KPGuerra96@gmail.com>
2020-12-30 14:27:20 -0500

    initial fetch

--- app/controllers/api/get_events_controller.rb
+++ app/controllers/api/get_events_controller.rb
@@ -3,7 +3,6 @@ class Api::GetEventsController < ApplicationController
 
 
     def index
-       
         response = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?city=New%20York&countryCode=US&segmentName=music&apikey=#{ENV['TM_KEY']}")
         events = JSON.parse(response)
         render json: events
