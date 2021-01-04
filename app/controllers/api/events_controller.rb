3ebdeeace82f230a2b40afac2c89be26f80ffbbd
KPGuerra <KPGuerra96@gmail.com>
2021-01-04 12:10:14 -0500

    Starting Saved Events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,5 +4,21 @@ class Api::EventsController < ApplicationController
         events = Event.all
         render json: events
     end
+
+    def create
+        event = Event.create!(event_params)
+
+        if event.valid?
+            render json: event
+        else
+            render json: {error: 'failed to save event'}, status: :not_acceptable 
+        end
+    end 
+
+    private
+
+    def event_params
+        params.require(:event).permit(:title, :artist, :venue, :date, :image)
+
     
 end
