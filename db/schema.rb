120c5eb36a729413d399b7e08addfc64cc51671a
KPGuerra <KPGuerra96@gmail.com>
2021-01-06 12:14:32 -0500

    Saved Events Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_203351) do
+ActiveRecord::Schema.define(version: 2021_01_06_161827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_203351) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "ticketmasterid"
     t.string "time"
+    t.string "url"
   end
 
   create_table "user_events", force: :cascade do |t|
